decb9907610495bc924a6464a140766f3d01c647
Ayesh Kavinda <ayeskavinda@gmail.com>
2026-05-02 21:06:27 +0530

    JuiceWrld.py
    All Girls Are the Same Lyrics

--- src/MyName.py
+++ src/MyName.py
@@ -1,1 +1,8 @@
-print('My Name is Ayeshh')
+a = 'All Girls Are the Same'
+b = 'They Are Rotting in My Brain'
+c = 'Think I Need a Change'
+d = 'Before I Go Insane'
+print(a)
+print(b)
+print(c)
+print(d)
